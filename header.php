5ac7c829745401d7ccfd87586e690fabab984f3c
artemka02kur <artem.kursev714@gmail.com>
2022-06-25 23:56:30 +0300

    new file

---
+++ header.php
@@ -0,0 +1,20 @@
+<header class="site-header">
+
+    <ul class = "h">
+      <a href="/"><li>Home</li></a>
+      <a href="/"><li>Case studies</li></a>
+      <a href="/"><li>Testimonials</li></a>
+      <a href="my_project.html"><li>My project</li></a>
+      <div class="introbutton"><li>
+        <div class="dropdown">
+<div class="dropbtn">Get in touch</div>
+<div class="dropdown-content">
+<a class="git" href="https://github.com/artemka02kur">  <img src="git.png" alt="git" class="scale"></a>
+<a class="vk" href="https://vk.com/id202603936"><img src="vk.png" alt="vk" class="scale"></a>
+<a class="inst" href="https://www.instagram.com/artiomkurshev2825/"><img src="inst.png" alt="inst" class="scale"></a>
+</div>
+</div>
+</li></div>
+    </ul>
+
+</header>
